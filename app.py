3cc2c42cc34f079d6d563377674b7314177627a0
LRania <LRania@users.noreply.github.com>
2021-06-10 18:13:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
-app = Flask(__name__)
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
 
-@app.route("/")
-def hello():
-	html = "<h3>Hello !</h3>"
-	return html
-	
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file.")
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
